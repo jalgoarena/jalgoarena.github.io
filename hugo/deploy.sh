53c63ff02f3dfb1e3f17844bcd9314dc3e5f7150
Jacek Spólnik <jacek.spolnik@gmail.com>
2018-07-20 20:02:51 +0200

    rebuilding site pią, 20 lip 2018, 20:02:51 CEST

---
+++ hugo/deploy.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+echo -e "\033[0;32mDeploying updates to GitHub...\033[0m"
+`hugo -t kube`
+
+cd ..
+git add .
+
+msg="rebuilding site `date`"
+if [ $# -eq 1 ]
+  then msg="$1"
+fi
+git commit -m "$msg"
+
+git push origin master
+cd hugo
